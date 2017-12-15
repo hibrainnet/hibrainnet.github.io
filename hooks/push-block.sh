5838c4d0f9e9e4e4d58ea034fc46988c64ac19c2
Pgahye <rkgp0515@gmail.com>
2017-12-15 17:15:46 +0900

    _config, hooks, tag, category update

--- hooks/push-block.sh
+++ hooks/push-block.sh
@@ -1,22 +1,23 @@
 
 
-
-
-
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 USER_EMAIL=$(git log -1 --format=format:%ae HEAD)
 USER_NAME=$(git log -1 --format=format:%an HEAD)
 
-#echo $USER_NAME
-#echo $branch
 
-if [ "$branch" = HBNINTERN-24-master-branch-pre-hooks ];
+ADMIN_EMAIL=$(grep -w 'ADMIN_EMAIL:' "$PWD"/_config.yml | tail -n1 | cut -c 13- | tr -d ' ' )
+
+
+echo $ADMIN_EMAIL
+echo $USER_EMAIL
+
+if [ "$branch" = master ];
 then
 
-    if [ "$USER_EMAIL" != rkgp05151@gmail.com ];
+    if [ "$USER_EMAIL" != "$ADMIN_EMAIL" ];
     then
         echo "-----------------------------------------------------"
-        echo "WARNING: You are not allowed to Push in MASTER branch !"
+        echo "WARNING: You are not allowed to Push/commit in MASTER branch !"
         echo "-----------------------------------------------------"
         exit 1
     fi
