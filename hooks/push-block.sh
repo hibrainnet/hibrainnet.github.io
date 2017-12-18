a9926e45e57cb145e27461fecb22a53ed39e7a06
Pgahye <rkgp0515@gmail.com>
2017-12-18 10:15:22 +0900

    script ruby add, hooks update

--- hooks/push-block.sh
+++ hooks/push-block.sh
@@ -4,17 +4,16 @@ branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 USER_EMAIL=$(git log -1 --format=format:%ae HEAD)
 USER_NAME=$(git log -1 --format=format:%an HEAD)
 
+ADMIN_GIT_EMAIL=$(ruby "$PWD"/script/config.rb "admin_git_email")
 
-ADMIN_EMAIL=$(grep -w 'ADMIN_EMAIL:' "$PWD"/_config.yml | tail -n1 | cut -c 13- | tr -d ' ' )
-
-
+echo $PWD
 echo $ADMIN_EMAIL
 echo $USER_EMAIL
 
 if [ "$branch" = master ];
 then
 
-    if [ "$USER_EMAIL" != "$ADMIN_EMAIL" ];
+    if [ "$USER_EMAIL" != "$ADMIN_GIT_EMAIL" ];
     then
         echo "-----------------------------------------------------"
         echo "WARNING: You are not allowed to Push/commit in MASTER branch !"
