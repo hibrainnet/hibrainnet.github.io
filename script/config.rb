75fa5dc4c852cafdd70ed7fffcd47c1155d4faff
Pgahye <rkgp0515@gmail.com>
2017-12-19 14:28:42 +0900

    many admin email update

--- script/config.rb
+++ script/config.rb
@@ -1,10 +1,8 @@
 require 'yaml'
 
-
 path = File.expand_path("../../", __FILE__)
 config = YAML.load_file(path+"/_config.yml")
-admin_git_email = config['git']['admin-git-email']
-
-puts admin_git_email
 
 
+@admin_git_email = config['git']['admins']
+@admin_length = @admin_git_email.length
