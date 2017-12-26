4a42775d861a99022307ab93bb85c3559110e822
JuHanHeo <herojoon1378@gmail.com>
2017-12-26 10:05:57 +0900

    test

--- hooks/push-github.sh
+++ hooks/push-github.sh
@@ -4,23 +4,17 @@ url="$2"
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 path=$(pwd)
 
-readYaml="$(ruby $path/hooks/read-yaml.rb)"
+readYaml="$(ruby $path/.git/hooks/read-yaml.rb)"
 
 remote_url="$(echo $readYaml | cut -d' ' -f1)"
+
 branch="$(echo $readYaml | cut -d' ' -f2)"
-remote_name="$(echo $readYaml | cut -d' ' -f3)"
 
-if [ -z "$remote_name" ]:
-then 
-	remote_name="github"
-fi 
-
-check="$(git remote get-url $remote_name)"
-if [ "$check" ]:; then echo "$remote Pushing....."; else git remote add $remote_name $remote_url && echo "Add first $remote_name URL"; fi
+token="$(echo $readYaml | cut -d' ' -f3)"
 
 if [ $remote = "origin" ]
 then
-   git push $remote_name $BRANCH:$branch
+   git push https://$token@github.com$remote_url $BRANCH:$branch
 fi
 
 exit $?
