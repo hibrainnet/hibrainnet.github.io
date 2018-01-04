75383e69018cd93e48c1bf813303481eac3b7740
jhheo <jhheo@jhheos-iMac.local>
2018-01-04 16:09:50 +0900

    test

--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
+echo hello
 git config --global user.email "herojoon1378@gmail.com"
 git config --global user.name "juhanheo"
 git remote add deploy https://github.com/hibrainnet/hibrainnet.github.io.git
